16e151bf2ab6442e8f820014862e865fb5bfa3dd
Shubham Mankar <mankarshubham81@gmail.com>
2025-01-26 12:56:43 +0530

    added env host

--- controllers/userController.js
+++ controllers/userController.js
@@ -176,7 +176,7 @@ class UserController {
         access_token_exp: accessTokenExp,
         is_auth: true
       });
-      res.redirect(`${FRONTEND_HOST}/user/task`);
+      res.redirect(`${process.env.FRONTEND_HOST}/user/task`);
 
 
     } catch (error) {
